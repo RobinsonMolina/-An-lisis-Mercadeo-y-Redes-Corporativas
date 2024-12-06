e6b0f431f90bc987c08cd0ba8dabdb35c6fa1790
ROBINSON MOLINA <robinson.granados01@gmail.com>
2024-12-06 12:02:42 -0500

    Added Node class

---
+++ src/main/java/co/edu/uptc/model/entities/Graph.java
@@ -0,0 +1,5 @@
+package co.edu.uptc.model.entities;
+
+public class Graph {
+
+}
---
+++ src/main/java/co/edu/uptc/model/entities/Node.java
@@ -0,0 +1,32 @@
+package co.edu.uptc.model.entities;
+
+public class Node {
+
+    private String id;
+    private String name;
+    private String type;
+
+    public Node(String id, String name, String type) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getname() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    @Override
+    public String toString() {
+        return "Node{" + "id='" + id + '\'' + ", name='" + name + '\'' + ", type='" + type + '\'' + '}';
+    }
+}
+
